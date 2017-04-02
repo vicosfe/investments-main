0c4694b059cd6835f8de38a81fc5adc4c75a09f2
Victor Osipov <victorosipov99@mail.ru>
2017-04-03 01:39:39 +0300

    add new 22 links

--- js/scripts.js
+++ js/scripts.js
@@ -9,5 +9,17 @@ $('.owl-carousel-header').owlCarousel({
 		smartSpeed: 1000,
 });
 
+/*$('.owl-carousel-media').owlCarousel({
+		items: 1,
+		dots: true,
+		loop: true,
+		autoplay: true,
+		nav: true,
+		autoplayTimeout: 6000,
+		smartSpeed: 1000,
+		animateOut: "slideOutUp",
+    	animateIn: "slideInUp"
+});*/
+
 
 });
