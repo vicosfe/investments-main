2fb872a86033e2421dad173627fc6f1753a70f69
Victor Osipov <victorosipov99@mail.ru>
2017-04-04 00:13:34 +0300

    create footer

--- js/scripts.js
+++ js/scripts.js
@@ -8,18 +8,15 @@ $('.owl-carousel-header').owlCarousel({
 		autoplayTimeout: 6000,
 		smartSpeed: 1000,
 });
+var swiper = new Swiper('.swiper-container', {
 
-/*$('.owl-carousel-media').owlCarousel({
-		items: 1,
-		dots: true,
-		loop: true,
-		autoplay: true,
-		nav: true,
-		autoplayTimeout: 6000,
-		smartSpeed: 1000,
-		animateOut: "slideOutUp",
-    	animateIn: "slideInUp"
-});*/
-
+        paginationClickable: true,
+        direction: 'vertical',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        speed: 1000,
+        loop: true,
+        autoplay: 4000
+    });
 
 });
